086a571308edef12352f163568991b8568083cde
ontheboxtf2 <nablamario@proton.me>
2022-10-02 18:41:24 -0400

    Setup build/deploy

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+	'build',
+	{
+		branch: 'gh-pages',
+		repo: 'https://https://github.com/ontheboxtf2/ontheboxtf2.github.io.git',
+		user: {
+		name: 'ontheboxtf2',
+			email: 'nablamario@proton.me'
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!')
+	}
+)
