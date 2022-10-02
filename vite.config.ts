08d0613a2ca60e12133319ba4a56e9583a021a32
ontheboxtf2 <nablamario@proton.me>
2022-10-02 17:50:14 -0400

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
     plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			plugins: [
+				visualizer({
+					filename: './generated/build-summary.html',
+					gzipSize: true,
+					template: 'treemap', // sunburst|treemap|network
+				}),
+			],
+		},
+	},
 }
 
 export default config
