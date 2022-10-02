e50a95e2a96cd041ab610aa92deee3d844d297c1
ontheboxtf2 <nablamario@proton.me>
2022-10-02 19:56:46 -0400

    Updates

---
+++ _app/immutable/chunks/1-7a4ea09c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-35d67014.js";export{t as component};
---
+++ _app/immutable/chunks/2-7743246e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ea495ec7.js";export{t as component};
