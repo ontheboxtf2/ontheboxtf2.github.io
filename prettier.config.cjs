f1e5720c3102aa1b64b9ed96e1773dd309af4e9c
ontheboxtf2 <nablamario@proton.me>
2022-10-02 17:36:05 -0400

    Add tailwind, configure prettier, format

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'always',
+}
